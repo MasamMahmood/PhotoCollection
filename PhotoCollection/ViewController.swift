6594ff5cbe99b30b7a96284ef8a66a5ad097f9f9
Masam Mahmood <masam.mahmood@tekhnelogos.com>
2019-12-19 14:57:52 +0300

    Initial Commit

---
+++ PhotoCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoCollection
+//
+//  Created by Masam Mahmood on 19.12.2019.
+//  Copyright © 2019 Masam Mahmood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
